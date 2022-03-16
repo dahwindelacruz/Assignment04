8c74b954435ec07946abf8930d5d62c9f388b6cf
Dahwin DelaCruz <dahwin.delacruz@gmail.com>
2022-03-16 13:30:26 -0700

    update

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,17 +1,38 @@
 //STEP 1
 // Write a function called halfNumber() that accepts one argument (a number), divide it by 2, and return the result. It should log a string like "Half of 5 is 2.5.".
 
-function halfNumber(numbers) {
-    let result = numbers / 2
-    console.log(`Half of ${numbers} is ${result}`)
-}
-halfNumber()
-halfNumber(6)
+// function halfNumber(numbers) {
+//     let result = numbers / 2
+//     console.log(`Half of ${numbers} is ${result}`)
+// }
+// halfNumber()
+// halfNumber(6)
+// halfNumber(38)
 
 
 //STEP 2
+// Write a function called squareNumber() that accepts one argument (a number), square that number, and return the result. It should also log a string like "The result of squaring the number 3 is 9."
+
+// function squareNumber(aNumber) {
+//     let result = Math.pow(aNumber, 2)
+//     console.log(`Half of ${aNumber} is ${result}`)
+// }
+// squareNumber()
+// squareNumber(6)
+// squareNumber(38)
+
 
 //STEP 3
+// Write a function called percentOf() that accepts two numbers, figure out what percent the first number represents of the second number, and return the result. It should log a string like "2 is 50% of 4."
+
+// function percentOf(firstNumber, secondNumber) {
+//     let modulus = firstNumber / secondNumber / Math.pow(10, -2)
+//     console.log(`${firstNumber} is ${modulus}% of ${secondNumber}`)
+// }
+
+// percentOf()
+// percentOf(5, 10)
+// percentOf(100, 4000)
 
 //STEP 4
 
