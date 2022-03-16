3fe55ba87c6e8ab41a84c9af78afa0f671aeb7cd
Dahwin DelaCruz <dahwin.delacruz@gmail.com>
2022-03-16 13:10:18 -0700

    initial commit

---
+++ js/01-functions-practice.js
@@ -0,0 +1,18 @@
+//STEP 1
+// Write a function called halfNumber() that accepts one argument (a number), divide it by 2, and return the result. It should log a string like "Half of 5 is 2.5.".
+
+function halfNumber(numbers) {
+    let result = numbers / 2
+    console.log(`Half of ${numbers} is ${result}`)
+}
+halfNumber()
+halfNumber(6)
+
+
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
